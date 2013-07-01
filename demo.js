82e00ee431c495524a43eaba8d88d8c41b39ccb0
Jim Blandy <jimb@red-bean.com>
2013-07-01 16:42:28 +0200

    final

--- demo.js
+++ demo.js
@@ -61,30 +61,23 @@ var demo = demo || {};
 demo.start();
 demo.log.length
 demo.stop()
-'script' in demo.log[0]
-demo.log[1]
-demo.log[2]
-'script' in demo.log[3]
 
 function findPlotWindow() {
   for (let b of gBrowser.browsers) {
     let w = b.contentWindow.wrappedJSObject;
-    if (w.document.URL.match(/\/demo.html$/)) {
+    if (w.document.URL.match(/\/plot.html$/)) {
       return w;
     }
   }
 }
 
-findPlotWindow()
-
 function plotCallLog() {
   var canvas = findPlotWindow().document.getElementById('callplot');
   var ctx = canvas.getContext('2d');
 
-  let width = canvas.width;
   let start = demo.log[0].time;
   let elapsed = demo.log[demo.log.length - 1].time - start;
-  function x(t) { return (t - start) * width / elapsed; }
+  function x(t) { return (t - start) * canvas.width / elapsed; }
   function y(d) { return d * 10; }
   function c(t) { return (t - start) * 720 / elapsed; }
 
@@ -92,7 +85,7 @@ function plotCallLog() {
     let rx = x(start);
     let ry = y(depth);
     ctx.fillStyle = "hsl(" + c(start) + ",80%,50%)";
-    ctx.fillRect(rx, ry, Math.ceil(x(end) - rx), y(depth + 1) - ry);
+    ctx.fillRect(rx, canvas.height - ry, Math.ceil(x(end) - rx), ry - y(depth + 1));
   }
 
   let stack = [];
@@ -100,17 +93,84 @@ function plotCallLog() {
   ctx.fillStyle = "rgb(255,255,255)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  if (1) {
-    for (e of demo.log) {
-      if ('script' in e) {
-        stack.push(e);
-      } else {
-        let call = stack.pop();
-        rect(call.time, e.time, stack.length);
-      }
+  for (e of demo.log) {
+    if ('script' in e) {
+      e.caller = stack.length > 0 ? stack[stack.length - 1] : null;
+      stack.push(e);
+    } else {
+      let call = stack.pop();
+      e.caller = stack.length > 0 ? stack[stack.length - 1] : null;
+      rect(call.time, e.time, stack.length);
     }
   }
-  console.log("JIMB: stack depth at end: " + stack.length);
 }
 
 plotCallLog()
+
+// calagator.org
+(function () {
+
+  var mouseMoveListener;
+  var posElt = findPlotWindow().document.getElementById('position');
+  var canvasElt = findPlotWindow().document.getElementById('callplot');
+  canvasElt.onmousemove = makeInfallible(onMouseMove);
+
+  function onMouseMove(event) {
+    let x = event.clientX - canvasElt.offsetLeft;
+    let y = event.clientY - canvasElt.offsetTop;
+
+    let start = demo.log[0].time;
+    let elapsed = demo.log[demo.log.length - 1].time - start;
+
+    let t = start + (x / canvasElt.width * elapsed);
+    let d = Math.floor((canvasElt.height - y) / 10);
+
+    console.log("JIMB: " + t + "," + d);
+    displayFrame(t, d);
+  }
+
+  function displayFrame(t, d) {
+    let e = findFrame(findStack(t), d);
+    if (e) {
+      posElt.firstChild.textContent =
+        uneval(e.script.url) + ":" +
+        e.script.getOffsetLine(e.offset);
+    } else {
+      posElt.firstChild.textContent = '<none>';
+    }
+  }
+
+  function findStack(t) {
+    let lower = 0, upper = demo.log.length - 1;
+    while (lower < upper) {
+      let mid = Math.ceil((lower + upper) / 2);
+      if (demo.log[mid].time <= t)
+        lower = mid;
+      else
+        upper = mid - 1;
+    }
+
+    return demo.log[lower];
+  }
+
+  function findFrame(s, d) {
+    var a = [];
+    for (;s; s = s.caller)
+      a.push(s);
+    if (d < a.length)
+      return a[a.length - 1 - d];
+    return null;
+  }
+
+  function makeInfallible(f) {
+    return function (...args) {
+      try {
+        return f.apply(this, args);
+      } catch (ex) {
+        console.log(f.name + " threw an exception: " + ex);
+        console.log(ex.stack);
+        return undefined;
+      }
+    }
+  }
+})();
---
+++ plot.js
@@ -0,0 +1,7 @@
+var positionElt = document.getElementById('position');
+
+// document.getElementById('callplot').onmousemove = function(event) {
+//    console.log("JIMB: " + event.screenX);
+// }
+
+var myglobal = 'foon';
